03408b4781148c330f0d956d871a990367a12237
David de Tena <daviddetenacalvo@gmail.com>
2015-04-08 10:29:09 +0200

    Initial Commit

---
+++ ConcurrenciaII/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConcurrenciaII
+//
+//  Created by David de Tena on 08/04/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
